608fe2df3bc6e5465b9ad86a3188a7be868771a1
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2016-05-31 05:56:05 +0000

    Make 'FlashCard' an interface
    In order to deal with different 'types' of flashcards (i.e. a 'standard' flashcard or 'languages' flashcard (for MFL subjects)), this new interface would be implemented by different flashcard classes to provide the basic functionality for any type of flashcard. These basic functions include having an id, question, answer, topic that the flashcard is from, and the 'type' of flashcard. The interface also implements `Parcelable` since flashcards will be passed between activities via intents.

--- app/src/main/java/com/satsumasoftware/flashcards/object/FlashCard.java
+++ app/src/main/java/com/satsumasoftware/flashcards/object/FlashCard.java
@@ -16,84 +16,28 @@
 
 package com.satsumasoftware.flashcards.object;
 
-import android.os.Parcel;
 import android.os.Parcelable;
-import android.text.Html;
+import android.support.annotation.StringDef;
 import android.text.Spanned;
 
-public class FlashCard implements Parcelable {
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 
-    private int mId, mPageRef;
-    private String mQuestion, mAnswer;
-    private Topic mTopic;
+public interface FlashCard extends Parcelable {
 
+    int getId();
 
-    public FlashCard(int id, String question, String answer, int pageRef, Topic topic) {
-        mId = id;
-        mQuestion = question;
-        mAnswer = answer;
-        mPageRef = pageRef;
-        mTopic = topic;
-    }
+    Spanned getQuestion();
 
+    Spanned getAnswer();
 
-    public int getId() {
-        return mId;
-    }
+    Topic getTopic();
 
-    public Spanned getQuestion() {
-        return Html.fromHtml(mQuestion);
-    }
+    @StringDef({STANDARD, LANGUAGE})
+    @Retention(RetentionPolicy.SOURCE)
+    @interface CourseType {}
+    String STANDARD = "standard", LANGUAGE = "language";
 
-    public Spanned getAnswer() {
-        if (mAnswer.equals("#see-guide")) {
-            return Html.fromHtml("This answer is too long to fit on a flashcard, but you can " +
-                    "check <b>page " + mPageRef + "</b> for details.");
-        } else {
-            return Html.fromHtml(mAnswer);
-        }
-    }
+    @CourseType String getFlashCardType();
 
-    public Topic getTopic() {
-        return mTopic;
-    }
-
-    public int getPageReference() {
-        return mPageRef;
-    }
-
-
-    protected FlashCard(Parcel in) {
-        mId = in.readInt();
-        mPageRef = in.readInt();
-        mQuestion = in.readString();
-        mAnswer = in.readString();
-        mTopic = in.readParcelable(Topic.class.getClassLoader());
-    }
-
-    public static final Creator<FlashCard> CREATOR = new Creator<FlashCard>() {
-        @Override
-        public FlashCard createFromParcel(Parcel in) {
-            return new FlashCard(in);
-        }
-
-        @Override
-        public FlashCard[] newArray(int size) {
-            return new FlashCard[size];
-        }
-    };
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeInt(mId);
-        dest.writeInt(mPageRef);
-        dest.writeString(mQuestion);
-        dest.writeString(mAnswer);
-        dest.writeParcelable(mTopic, flags);
-    }
 }
